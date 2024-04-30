6168b2d15ce6aada25cca38552dafd176bbdcbee
PiotrM <162429499+PiotrGreen@users.noreply.github.com>
2024-04-30 09:29:22 +0200

    Update customers.py

--- customers.py
+++ customers.py
@@ -1,5 +1,6 @@
 import csv
 import random
+import os
 
 
 def dane_klienta():
@@ -12,16 +13,21 @@ def dane_klienta():
     return imie, email, telefon, ulica, miasto, kraj
 
 
+moja_sciezka = ""
+
 def dodaj_klienta(fun):
     imie, email, telefon, ulica, miasto, kraj = fun
     id = random.randint(1000, 9999)
     with open('customer.csv', 'a', newline='', encoding='utf-8') as file:
         writer = csv.writer(file)
         writer.writerow([id, imie, email, telefon])
-    print("Nowy klient dodany do bazy.")
     with open('address.csv', 'a', newline='', encoding='utf-8') as file1:
         write = csv.writer(file1)
         write.writerow([id, ulica, miasto, kraj])
-
+    print("Nowy klient dodany do bazy.")
+    
+    filepath = os.path.join(moja_sciezka, '{}.txt'.format(id))
+    with open(filepath, 'w') as file2:
+        file2.write("Wypożyczone książki: {}\n".format(id))
 
 # dodaj_klienta(dane_klienta())
